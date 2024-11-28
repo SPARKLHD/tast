fa0daab50aba93517c1aa51d4b47e06883d9bb10
matsvetkova <121@>
2024-11-28 09:18:36 +0300

    task6

--- src/main/java/org/softwaretechnologies/MilkCofe.java
+++ src/main/java/org/softwaretechnologies/MilkCofe.java
@@ -1,10 +1,24 @@
 package org.softwaretechnologies;
 
-public class MilkCofe  {
-
+public class MilkCofe implements CofeInterface{
     /**
      * Создайте класс MilkCofe, поддерживающий интерфейс CofeIntrface
      * к стоимости базового напитка добавьте 10.
      * к описанию добавьте " + milk"
      */
+    CofeInterface cofeInterface;
+
+    public MilkCofe(CofeInterface cofeInterface) {
+        this.cofeInterface = cofeInterface;
+    }
+
+    @Override
+    public int getCost() {
+        return cofeInterface.getCost()+10;
+    }
+
+    @Override
+    public String description() {
+        return cofeInterface.description()+" + milk";
+    }
 }
--- src/main/java/org/softwaretechnologies/SugarCofe.java
+++ src/main/java/org/softwaretechnologies/SugarCofe.java
@@ -1,9 +1,22 @@
 package org.softwaretechnologies;
 
-public class SugarCofe {
+public class SugarCofe implements CofeInterface {
     /**
-     * Создайте класс MilkCofe, поддерживающий интерфейс CofeIntrface
+     * Создайте класс SugarCofe, поддерживающий интерфейс CofeIntrface
      * к стоимости базового напитка добавьте 20.
      * к описанию добавьте " + sugar"
      */
+    CofeInterface cofeInterface;
+    public SugarCofe(CofeInterface cofeInterface) {
+        this.cofeInterface = cofeInterface;
+    }
+    @Override
+    public int getCost() {
+        return cofeInterface.getCost()+20;
+    }
+
+    @Override
+    public String description() {
+        return cofeInterface.description()+"+ sugar";
+    }
 }
